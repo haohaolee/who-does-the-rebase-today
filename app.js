090ce3f3b2f176a3a4ffddc211db574b47be1ea0
Hao Li <hao.li@vodafone.com.au>
2016-06-27 21:46:44 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// app.js
+const Koa = require('koa');
+const app = new Koa();
+let moment = require('moment');
+
+const arrangement = [
+    'hao', 'max', 'biao', 'hao', 'max', 'Sat', 'Sun',
+    'biao', 'hao', 'max', 'biao', 'hao', 'Sat', 'Sun',
+    'max', 'biao', 'hao', 'max', 'biao', 'Sat', 'Sun'
+];
+
+const start = moment("2016-06-26 00:00 +0800", "YYYY-MM-DD HH:mm Z");
+
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+
+// response
+app.use(ctx => {
+  const now = moment();
+  const days = now.diff(start, 'days');
+
+  console.log(`Days: ${days}`);
+
+  let who = arrangement[days % arrangement.length]
+
+  console.log(`Who: ${who}`);
+
+  if (['Sat', 'Sun'].indexOf(who) != -1) {
+      ctx.body = `No one does rebase today because it is ${who} today`
+  }
+  else {
+      ctx.body = `${who} does rebase today`;
+  }
+});
+
+app.listen(3000);
+
